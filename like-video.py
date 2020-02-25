cac191cffacbfaf29f76c94e8ff0762da7670365
Jodie Kurnia <54063383+jodiekurnia@users.noreply.github.com>
2020-02-25 15:07:57 +0700

    update

---
+++ like-video.py
@@ -0,0 +1,9 @@
+from Bot import Bot
+
+# your location to chromedriver executable
+bot = Bot('chromedriver.exe')
+
+bot.goto_youtube() \
+    .login(email='your@jkogan@mail.ccsf.edu', password='1Sampai5!') \
+    .like(videoId='rESdwzBJRtw', watchFullVideo=True) \
+    .end()
